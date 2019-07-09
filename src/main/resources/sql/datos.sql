76c14e642ea73b8fcc7fbc9506b2651e79e855bd
Julian Osorio <julian.osorio@boos.cloud>
2019-07-09 13:58:19 -0500

    datos para la BD

---
+++ src/main/resources/sql/datos.sql
@@ -0,0 +1,53 @@
+/**
+ * Author:  josorio
+ * Created: 9/07/2019
+ */
+insert into tipificador
+(nombre, tipo)
+values ('Cesar','MARCA_REGIONAL'),
+('La Guajira','MARCA_REGIONAL'),
+('Magdalena','MARCA_REGIONAL'),
+('Atlantico','MARCA_REGIONAL'),
+('Bolívar','MARCA_REGIONAL'),
+('Sucre','MARCA_REGIONAL'),
+('Atlantico','MARCA_REGIONAL'),
+('Córdova','MARCA_REGIONAL'),
+('Antioquia','MARCA_REGIONAL'),
+('Chocó','MARCA_REGIONAL'),
+('Caldas','MARCA_REGIONAL'),
+('Risaralda','MARCA_REGIONAL'),
+('Quindío','MARCA_REGIONAL'),
+('Valle del Cauca','MARCA_REGIONAL'),
+('Tolima','MARCA_REGIONAL'),
+('Norte de Santander','MARCA_REGIONAL'),
+('Santander','MARCA_REGIONAL'),
+('Boyacá','MARCA_REGIONAL'),
+('Cundinamarca','MARCA_REGIONAL'),
+('Bogotá','MARCA_REGIONAL'),
+('Huila','MARCA_REGIONAL'),
+('Meta','MARCA_REGIONAL'),
+('Casanare','MARCA_REGIONAL'),
+('Arauca','MARCA_REGIONAL'),
+('Vichada','MARCA_REGIONAL'),
+('Guainía','MARCA_REGIONAL'),
+('Vaupés','MARCA_REGIONAL'),
+('Guaviare','MARCA_REGIONAL'),
+('Caquetá','MARCA_REGIONAL'),
+('Cauca','MARCA_REGIONAL'),
+('Nariño','MARCA_REGIONAL'),
+('Putumayo','MARCA_REGIONAL'),
+('Amazonas','MARCA_REGIONAL'),
+('','MARCA_GRAMATICAL'),
+('','MARCA_USO'),
+('','FUENTE'),
+
+insert into lema
+(texto, sufijo)
+values ('Reblujo', ''),
+('Reguero', ''),
+('Abanico', ''),
+('Abombado', 'da'),
+('China', ''),
+('Pepena', ''),
+('Sopladera', ''),
+('Ventiadora', '');
